1bca2d9cdea370e769ab30499d2c4977b8d738b5
Armando Lüscher <armando@noplanman.ch>
2016-09-05 13:55:07 +0200

    Enclose all variables in ${}. Adjust grep to only read unique URLs, add the ignored count to the final message.

--- git-svn-migrate.sh
+++ git-svn-migrate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Copyright 2010-2011 John Albin Wilkins and contributors.
+# Copyright 2016 Armando Lüscher.
 # Available under the GPL v2 license. See LICENSE.txt.
 
 script=$(basename $0);
@@ -15,25 +16,25 @@ git_author='Armando Lüscher <armando@noplanman.ch>';
 _git='git';
 _echo='echo';
 
-stdout_file="$dir/log/$(date +%Y%m%d%H%M%S).std.out";
-stderr_file="$dir/log/$(date +%Y%m%d%H%M%S).std.err";
+stdout_file="${dir}/log/$(date +%Y%m%d%H%M%S).std.out";
+stderr_file="${dir}/log/$(date +%Y%m%d%H%M%S).std.err";
 
 usage=$(cat <<EOF_USAGE
-USAGE: $script --url-file=<filename> --authors-file=<filename> [destination folder]
+USAGE: ${script} --url-file=<filename> --authors-file=<filename> [destination folder]
 \n
-\nFor more info, see: $script --help
+\nFor more info, see: ${script} --help
 EOF_USAGE
 );
 
 help=$(cat <<EOF_HELP
 NAME
-\n\t$script - Migrates Subversion repositories to Git
+\n\t${script} - Migrates Subversion repositories to Git
 \n
 \nSYNOPSIS
-\n\t$script [options] [arguments]
+\n\t${script} [options] [arguments]
 \n
 \nDESCRIPTION
-\n\tThe $script utility migrates a list of Subversion
+\n\tThe ${script} utility migrates a list of Subversion
 \n\trepositories to Git using the specified authors list. The
 \n\turl-file and authors-file parameters are required. The
 \n\tdestination folder is optional and can be specified as an
@@ -87,10 +88,10 @@ NAME
 \n
 \nBASIC EXAMPLES
 \n\t# Use the long parameter names
-\n\t$script --url-file=my-repository-list.txt --authors-file=authors-file.txt --destination=/var/git
+\n\t${script} --url-file=my-repository-list.txt --authors-file=authors-file.txt --destination=/var/git
 \n
 \n\t# Use short parameter names
-\n\t$script -u my-repository-list.txt -a authors-file.txt /var/git
+\n\t${script} -u my-repository-list.txt -a authors-file.txt /var/git
 \n
 \nSEE ALSO
 \n\tgit-svn-migrate-nohup.sh
@@ -101,14 +102,14 @@ EOF_HELP
 
 # Truly quiet git execution. (idea from http://stackoverflow.com/a/8944284)
 quiet_git() {
-  return $(git "$@" </dev/null >>$stdout_file 2>>$stderr_file);
+  return $(git "$@" </dev/null >>${stdout_file} 2>>${stderr_file});
 }
 
 # Output the "Done!" message after a step has completed.
 echo_done() {
   msg="${1:-Done.}";
-  echo "   $msg" >&2;
-  if [[ $_echo == "echo" ]]; then
+  echo "   ${msg}" >&2;
+  if [[ ${_echo} == "echo" ]]; then
     echo >&2;
   fi
 }
@@ -127,12 +128,12 @@ until [[ -z "$1" ]]; do
     fi
   else
     # Any argument given is assumed to be the destination folder.
-    tmp="destination=$option";
+    tmp="destination=${option}";
   fi
   parameter=${tmp%%=*}; # Extract option's name.
   value=${tmp##*=};     # Extract option's value.
   # If a value is expected, but not specified inside the parameter, grab the next param.
-  if [[ $value == $tmp ]]; then
+  if [[ ${value} == ${tmp} ]]; then
     if [[ ${2:0:1} == '-' ]]; then
       # The next parameter is a new option, so unset the value.
       value='';
@@ -141,27 +142,27 @@ until [[ -z "$1" ]]; do
     fi
   fi
 
-  case $parameter in
-    u|url-file )      url_file=$value;;
-    a|authors-file )  authors_file=$value;;
-    d|destination )   destination=$value;;
-    i|ignore-file )   ignore_file=$value;;
+  case ${parameter} in
+    u|url-file )      url_file=${value};;
+    a|authors-file )  authors_file=${value};;
+    d|destination )   destination=${value};;
+    i|ignore-file )   ignore_file=${value};;
     f|force )         force=1;;
     q|quiet )         _git="quiet_git"; _echo="echo -n";;
-    shared )          if [[ $value == '' ]]; then
+    shared )          if [[ ${value} == '' ]]; then
                         gitinit_params="--shared";
                       else
-                        gitinit_params="--shared=$value";
+                        gitinit_params="--shared=${value}";
                       fi
                       ;;
 
-    h|help )          echo -e $help | less >&2; exit;;
+    h|help )          echo -e ${help} | less >&2; exit;;
 
     * ) # Pass any unknown parameters to git-svn directly.
-        if [[ $value == '' ]]; then
-          gitsvn_params="$gitsvn_params $flag_delimiter$parameter";
+        if [[ ${value} == '' ]]; then
+          gitsvn_params="${gitsvn_params} ${flag_delimiter}${parameter}";
         else
-          gitsvn_params="$gitsvn_params $flag_delimiter$parameter=$value";
+          gitsvn_params="${gitsvn_params} ${flag_delimiter}${parameter}=${value}";
         fi;;
   esac
 
@@ -170,19 +171,19 @@ until [[ -z "$1" ]]; do
 done
 
 # Check for required parameters.
-if [[ $url_file == '' || $authors_file == '' ]]; then
-  echo -e $usage >&2;
+if [[ ${url_file} == '' || ${authors_file} == '' ]]; then
+  echo -e ${usage} >&2;
   exit 1;
 fi
 # Check for valid files.
-if [[ ! -f $url_file ]]; then
-  echo "Specified URL file \"$url_file\" does not exist or is not a file." >&2;
-  echo -e $usage >&2;
+if [[ ! -f ${url_file} ]]; then
+  echo "Specified URL file \"${url_file}\" does not exist or is not a file." >&2;
+  echo -e ${usage} >&2;
   exit 1;
 fi
-if [[ ! -f $authors_file ]]; then
-  echo "Specified authors file \"$authors_file\" does not exist or is not a file." >&2;
-  echo -e $usage >&2;
+if [[ ! -f ${authors_file} ]]; then
+  echo "Specified authors file \"${authors_file}\" does not exist or is not a file." >&2;
+  echo -e ${usage} >&2;
   exit 1;
 fi
 
@@ -193,15 +194,15 @@ mkdir -p "$destination";
 destination=$(cd "$destination"; pwd); #Absolute path.
 
 # Ensure temporary repository location is empty.
-if [[ -e $tmp_destination ]] && [[ $force -eq 0 ]]; then
-  echo "Temporary repository location \"$tmp_destination\" already exists. Exiting." >&2;
+if [[ -e ${tmp_destination} ]] && [[ ${force} -eq 0 ]]; then
+  echo "Temporary repository location \"${tmp_destination}\" already exists. Exiting." >&2;
   exit 1;
 fi
 
 # http://stackoverflow.com/a/114861
 # http://stackoverflow.com/a/114836
 # Ignore empty lines and commented lines (with # or ;)
-cnt_total=$(grep -vcE '^$|^[#;]' "$url_file");
+cnt_total=$(grep -vcE '^$|^[#;]' "${url_file}");
 cnt_cur=0;
 cnt_pass=0;
 cnt_skip=0;
@@ -213,43 +214,43 @@ do
   skipping=0;
 
   # Check for 2-field format:  Name [tab] URL
-  name=$(echo $line | awk '{print $1}');
-  url=$(echo $line | awk '{print $2}');
+  name=$(echo ${line} | awk '{print $1}');
+  url=$(echo ${line} | awk '{print $2}');
 
   # Check for simple 1-field format:  URL
-  if [[ $url == '' ]]; then
-    url=$name;
-    name=$(basename $url);
+  if [[ ${url} == '' ]]; then
+    url=${name};
+    name=$(basename ${url});
   fi
 
   # The directory where the new git repository is going.
-  destination_git="$destination/$name.git";
+  destination_git="$destination/${name}.git";
 
   # Process each Subversion URL.
   echo >&2;
-  echo "( $cnt_cur / $cnt_total ) At $(date)..." >&2;
-  echo "Processing \"$name\" repository:" >&2;
-  echo " < $url" >&2;
-  echo " > $destination_git" >&2;
+  echo "( $cnt_cur / ${cnt_total} ) At $(date)..." >&2;
+  echo "Processing \"${name}\" repository:" >&2;
+  echo " < ${url}" >&2;
+  echo " > ${destination_git}" >&2;
   echo >&2;
 
   # Init the final bare repository.
   # Ensure temporary repository location is empty.
-  if [[ -e "$destination_git" ]] && [[ $force -eq 0 ]]; then
-    echo " - Repository location \"$destination_git\" already exists. Skipping." >&2;
+  if [[ -e "${destination_git}" ]] && [[ ${force} -eq 0 ]]; then
+    echo " - Repository location \"${destination_git}\" already exists. Skipping." >&2;
     skipping=1;
   fi
 
-  if [[ $skipping -eq 0 ]]; then
-    mkdir -p "$destination_git";
-    cd "$destination_git";
-    $_git init --bare $gitinit_params $gitsvn_params;
-    $_git symbolic-ref HEAD refs/heads/trunk $gitsvn_params;
+  if [[ ${skipping} -eq 0 ]]; then
+    mkdir -p "${destination_git}";
+    cd "${destination_git}";
+    ${_git} init --bare ${gitinit_params} ${gitsvn_params};
+    ${_git} symbolic-ref HEAD refs/heads/trunk ${gitsvn_params};
 
     # Clone the original Subversion repository to a temp repository.
-    cd "$dir";
-    $_echo " - Cloning repository..." >&2;
-    $_git svn clone "$url" -A "$authors_file" --authors-prog="$dir/svn-lookup-author.sh" --stdlayout --quiet "$tmp_destination" $gitsvn_params;
+    cd "${dir}";
+    ${_echo} " - Cloning repository..." >&2;
+    ${_git} svn clone "${url}" -A "${authors_file}" --authors-prog="${dir}/svn-lookup-author.sh" --stdlayout --quiet "${tmp_destination}" ${gitsvn_params};
     if [[ $? -eq 0 ]]; then
       echo_done;
     else
@@ -258,68 +259,68 @@ do
     fi
   fi
 
-  if [[ $skipping -eq 0 ]]; then
+  if [[ ${skipping} -eq 0 ]]; then
     # Create .gitignore file.
-    $_echo " - svn:ignore => .gitignore file..." >&2;
-    if [[ $ignore_file != '' ]]; then
-      cp "$ignore_file" "$tmp_destination/.gitignore";
+    ${_echo} " - svn:ignore => .gitignore file..." >&2;
+    if [[ ${ignore_file} != '' ]]; then
+      cp "${ignore_file}" "${tmp_destination}/.gitignore";
     fi
-    cd "$tmp_destination";
-    $_git svn show-ignore --id trunk >> .gitignore;
-    $_git add .gitignore;
-    $_git commit --author="$git_author" -m 'Convert svn:ignore properties to .gitignore.' $gitsvn_params;
+    cd "${tmp_destination}";
+    ${_git} svn show-ignore --id trunk >> .gitignore;
+    ${_git} add .gitignore;
+    ${_git} commit --author="${git_author}" -m 'Convert svn:ignore properties to .gitignore.' ${gitsvn_params};
     #git commit --author="git-svn-migrate <nobody@example.org>" -m 'Convert svn:ignore properties to .gitignore.';
     echo_done;
 
     # Push to final bare repository and remove temp repository.
-    $_echo " - Pushing to new bare repository..." >&2;
-    $_git remote add bare "$destination_git";
-    $_git config remote.bare.push 'refs/remotes/*:refs/heads/*';
-    $_git push bare $gitsvn_params;
+    ${_echo} " - Pushing to new bare repository..." >&2;
+    ${_git} remote add bare "${destination_git}";
+    ${_git} config remote.bare.push 'refs/remotes/*:refs/heads/*';
+    ${_git} push bare ${gitsvn_params};
     # Push the .gitignore commit that resides on master.
-    $_git push bare master:trunk $gitsvn_params;
-    cd "$dir";
-    rm -r "$tmp_destination";
+    ${_git} push bare master:trunk ${gitsvn_params};
+    cd "${dir}";
+    rm -r "${tmp_destination}";
     echo_done;
 
-    $_echo " - Fix branches..." >&2;
+    ${_echo} " - Fix branches..." >&2;
     # Rename Subversion's "trunk" branch to Git's standard "master" branch.
-    cd "$destination_git";
-    $_git branch -m trunk master;
+    cd "${destination_git}";
+    ${_git} branch -m trunk master;
     # Remove bogus branches of the form "name@REV".
-    $_git for-each-ref --format='%(refname)' refs/heads | grep '@[0-9][0-9]*' | cut -d / -f 3- |
+    ${_git} for-each-ref --format='%(refname)' refs/heads | grep '@[0-9][0-9]*' | cut -d / -f 3- |
     while read ref
     do
-      $_git branch -D "$ref";
+      ${_git} branch -D "$ref";
     done
     echo_done;
 
     # Convert git-svn tag branches to proper tags.
-    $_echo " - SVN tags => git tags..." >&2;
-    $_git for-each-ref --format='%(refname)' refs/heads/tags | cut -d / -f 4 |
+    ${_echo} " - SVN tags => git tags..." >&2;
+    ${_git} for-each-ref --format='%(refname)' refs/heads/tags | cut -d / -f 4 |
     while read ref
     do
-      $_git tag -a "$ref" -m "Convert \"$ref\" to a proper git tag." "refs/heads/tags/$ref";
-      $_git branch -D "tags/$ref";
+      ${_git} tag -a "$ref" -m "Convert \"$ref\" to a proper git tag." "refs/heads/tags/$ref";
+      ${_git} branch -D "tags/$ref";
     done
     echo_done;
     echo >&2;
 
-    echo "Conversion of \"$name\" completed at $(date)." >&2;
+    echo "Conversion of \"${name}\" completed at $(date)." >&2;
     ((cnt_pass++));
   else
     echo >&2;
-    echo "Conversion of \"$name\" skipped at $(date)." >&2;
+    echo "Conversion of \"${name}\" skipped at $(date)." >&2;
     ((cnt_skip++));
   fi
-done < <(grep -vE '^$|^[#;]' "$url_file")
+done < <(grep -vE '^$|^[#;]' "${url_file}" | nl -w14 -nrz -s, | sort -t, -k2 -u | sort -n | cut -d, -f2-)
 # http://stackoverflow.com/a/8197412
 # http://mywiki.wooledge.org/BashFAQ/024 (ProcessSubstitution)
 
 echo >&2;
-echo "All done! ( $cnt_pass / $cnt_total passed )" >&2
+echo "All done! Total: ${cnt_total} (${cnt_pass} passed, ${cnt_skip} skipped, $((cnt_total - cnt_cur)) ignored)" >&2
 echo >&2;
-if [[ $cnt_skip -ne 0 ]]; then
-  echo "($cnt_skip conversions were skipped, check the output and logs)" >&2
+if [[ ${cnt_skip} -ne 0 ]]; then
+  echo "(${cnt_skip} conversions were skipped, check the output and logs)" >&2
   echo >&2;
 fi
